296a1965e61aca110c5be39e2c974345e1fc04f9
patilkrunal <patilkrunal11.kp@gmail.com>
2022-02-04 01:15:16 +0530

    push build folder

---
+++ frontend/build/precache-manifest.dfdd8a5ff9115650863e57f66531952d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a6352b22f4571895950eb660039db5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e68da345a6f06e68b4d",
+    "url": "./static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "42c4e0bbe4bbb8391751",
+    "url": "./static/css/main.ea169596.chunk.css"
+  },
+  {
+    "revision": "7e68da345a6f06e68b4d",
+    "url": "./static/js/2.8bd69473.chunk.js"
+  },
+  {
+    "revision": "57d70d815e7f0b76718e265581a09ff9",
+    "url": "./static/js/2.8bd69473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c4e0bbe4bbb8391751",
+    "url": "./static/js/main.485e7886.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
